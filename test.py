3f80732e1cb6ce549a8c57b47dbbd5eb497a3ae1
dave peel <davidpeel33@hotmail.com>
2024-02-13 21:41:29 +0000

    Testing feature to add filter to trade count by client and getting dates list from trade table
    This commit adds two features. One to the trade_count_by_client function. It now allows for it to be passed in the True bool if the function is to be used to run on one specific trade date.
    It utilises code structure defined in the netting_summary_by_value_date where the get_available_report_value_dates function length is checked and returns if there is no data available.
    It then checks if the bool flag is set to true and if so will initialise the fuzzy_select_menu from utils and setting the date variable, using this to filter the pandas data frame.
    
    The helper function of get_available_report_value_dates has been also refactored to allow for selection of trade of value, so it can serve multiple uses.
    
    **Potential further refactor is to define the table names into a constant variable file so that these are not hard coded into the functions themselves. Allowing for better re-usability.

--- test.py
+++ test.py
@@ -116,6 +116,7 @@
 
 
 import os
+import time
 
 from pandas import pandas as pd
 from rich import print as rprint
@@ -123,6 +124,7 @@ from rich.console import Console
 from rich.table import Table
 
 from lib.database import FX_NET_DB_TRADES_TABLE
+from lib import utils
 
 
     
@@ -150,4 +152,62 @@ def trade_count_by_client_and_trader(trade_date_filter=None):
     rprint("[cyan]Scroll to see full table if required")
     input("Press Enter to continue")
 
-trade_count_by_client_and_trader()
+# trade_count_by_client_and_trader()
+
+def trade_count_by_client(trade_date_filter=False):
+    os.system("clear")
+    trades_data = FX_NET_DB_TRADES_TABLE.get_all_values()
+    df = pd.DataFrame(trades_data[1:],columns=trades_data[0])
+
+    dates = get_available_report_dates_by_type("trade")
+    date = "All Trade Dates"
+    if len(dates) == 0:
+        rprint("[red]No data stored in FX Net Database")
+        rprint("[red]Please load data or select another option")
+        time.sleep(2)
+    else:
+        if trade_date_filter:
+            date = utils.fuzzy_select_menu("Please select a date", dates)
+            print(date)
+            df = df[df['TRADE_DATE'] == date]
+
+        client_trade_counts = list(df['CLIENT_NAME'].value_counts().items())
+
+        trade_count_table = Table(title=f"\n\nTrade Count by Client - {date}")
+        trade_count_table.add_column("Client Name", justify="center", style="white")
+        trade_count_table.add_column("Count of Trades Booked", justify="center", style="white")
+
+        for client, count_of_trades in client_trade_counts:
+            trade_count_table.add_row(client,
+                                    str(count_of_trades))
+        console = Console()
+        console.print(trade_count_table)
+
+        rprint("[cyan]Scroll to see full table if required")
+        input("Press Enter to continue")
+
+def get_available_report_dates_by_type(value_or_trade):
+    '''
+    Helper function to pull out all available dates by type 
+    from the TRADES table.
+
+    Parameters: Either trade or value passed in as a string
+
+    Returns: List of value dates or an empty list
+    '''
+    if value_or_trade == "value":
+        date_type = "VALUE_DATE"
+    else:
+        date_type = "TRADE_DATE"
+
+    all_data = FX_NET_DB_TRADES_TABLE.get_all_values()
+    df = pd.DataFrame(all_data[1:],columns=all_data[0])
+
+    value_dates = df[date_type].unique()
+    if len(value_dates) > 0:
+        return value_dates
+    else:
+        return []
+
+
+trade_count_by_client()
